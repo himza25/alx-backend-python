1e67ed13d5bf328722f61a2f863bbc784a8c8888
himza25 <himza.of.theking95@gmail.com>
2024-04-18 12:07:38 -0700

    task 4 & 5

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module with annotated variables demonstrating basic types."""
+
+
+a: int = 1  # An integer variable
+pi: float = 3.14  # A float variable
+i_understand_annotations: bool = True  # A boolean variable
+school: str = "Holberton"  # A string variable
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for a function that sums a list of floats."""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Sum a list of floats and return the sum."""
+    return sum(input_list)
