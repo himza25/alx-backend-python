2fa57a4f1e5a549e1ac70b65af1a6b7d039c478b
himza25 <himza.of.theking95@gmail.com>
2024-04-18 12:20:22 -0700

    task 8 & 9

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,13 +5,13 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Create and return a function that multiplies its input by a fixed number.
+    """Create and return a function multiplies its input by a fixed number.
 
     Args:
         multiplier (float): The number to multiply by.
 
     Returns:
-        Callable[[float], float]: A function that multiplies its input by `multiplier`.
+        Callable[[float], float]: A function multiplies its input by `multiplier`.
     """
     def multiplier_func(value: float) -> float:
         """Multiply the input value by the multiplier."""
