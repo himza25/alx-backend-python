8d65191db65242609a43be98e6b4ad5dbf20e4a7
himza25 <himza.of.theking95@gmail.com>
2024-04-18 12:01:16 -0700

    task 2 & 3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module with a type-annotated function to compute the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """Return the floor of the float n."""
+    from math import floor
+    return floor(n)
