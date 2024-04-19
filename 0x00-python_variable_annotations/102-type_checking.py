b107b4235bf3ad9140e37401b5bc91373a7c786d
himza25 <himza.of.theking95@gmail.com>
2024-04-18 19:30:01 -0700

    task 12

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,16 +7,7 @@ from typing import Tuple, List, Any
 
 
 def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
-    """
-    Replicate each element in a tuple a given number of times.
-
-    Args:
-        lst (Tuple[Any, ...]): Tuple from which elements are replicated.
-        factor (int): Number of times to replicate each element.
-
-    Returns:
-        List[Any]: List containing replicated elements.
-    """
+    ''' Replicate each element in a tuple a given number of times.'''
     zoomed_in: List[Any] = [
         item for item in lst
         for _ in range(factor)
