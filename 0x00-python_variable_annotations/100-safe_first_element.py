414dac07db3f3f37bbe7d4ede04ca75b03204f3c
himza25 <himza.of.theking95@gmail.com>
2024-04-18 18:43:02 -0700

    task 10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module to safely return the first element of a sequence."""
+
+from typing import Sequence, Any, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Return the first element of a sequence or None if the sequence is empty.
+
+    Args:
+        lst (Sequence[Any]): The sequence from which to get the first element.
+
+    Returns:
+        Optional[Any]: The first element of the sequence or None if it's empty.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
